dc3f337273b8252cc3ee021b96667b90e6c4837d
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-12-23 12:32:54 -0600

    Create MathFunctions.lua

---
+++ LuaORLuau/RobloxLuau/MathFunctions.lua
@@ -0,0 +1,13 @@
+-- This script meant to be a module
+-- Scripted by master200crate on Roblox. Other accounts: SY-Gato (Github)
+local api = {}
+local euler = 2.718281
+
+function api.dot2items(list1,list2)
+  first_indexes_mult = input_vector[0] * weights_1[0]
+  second_indexes_mult = input_vector[1] * weights_1[1]
+  return first_indexes_mult + second_indexes_mult
+
+function
+
+return api
