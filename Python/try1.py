4df24843df1ad635f3a332b7d02782977de7025d
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2024-01-04 11:30:58 -0600

    Update try1.py

--- Python/try1.py
+++ Python/try1.py
@@ -1,7 +1,48 @@
 import numpy as numpy
+import matplotlib.pyplot as plt
 
 class NeuralNetwork:
-    # ...
+    def __init__(self, learning_rate):
+        self.weights = np.array([np.random.randn(), np.random.randn()])
+        self.bias = np.random.randn()
+        self.learning_rate = learning_rate
+
+    def _sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def _sigmoid_deriv(self, x):
+        return self._sigmoid(x) * (1 - self._sigmoid(x))
+
+    def predict(self, input_vector):
+        layer_1 = np.dot(input_vector, self.weights) + self.bias
+        layer_2 = self._sigmoid(layer_1)
+        prediction = layer_2
+        return prediction
+
+    def _compute_gradients(self, input_vector, target):
+        layer_1 = np.dot(input_vector, self.weights) + self.bias
+        layer_2 = self._sigmoid(layer_1)
+        prediction = layer_2
+
+        derror_dprediction = 2 * (prediction - target)
+        dprediction_dlayer1 = self._sigmoid_deriv(layer_1)
+        dlayer1_dbias = 1
+        dlayer1_dweights = (0 * self.weights) + (1 * input_vector)
+
+        derror_dbias = (
+            derror_dprediction * dprediction_dlayer1 * dlayer1_dbias
+        )
+        derror_dweights = (
+            derror_dprediction * dprediction_dlayer1 * dlayer1_dweights
+        )
+
+        return derror_dbias, derror_dweights
+
+    def _update_parameters(self, derror_dbias, derror_dweights):
+        self.bias = self.bias - (derror_dbias * self.learning_rate)
+        self.weights = self.weights - (
+            derror_dweights * self.learning_rate
+        )
 
     def train(self, input_vectors, targets, iterations):
         cumulative_errors = []
@@ -36,23 +77,22 @@ class NeuralNetwork:
         return cumulative_errors
 
 
-In [45]: # Paste the NeuralNetwork class code here
-   ...: # (and don't forget to add the train method to the class)
 
-In [46]: import matplotlib.pyplot as plt
 
-In [47]: input_vectors = np.array(
-   ...:     [
-   ...:         [3, 1.5],
-   ...:         [2, 1],
-   ...:         [4, 1.5],
-   ...:         [3, 4],
-   ...:         [3.5, 0.5],
-   ...:         [2, 0.5],
-   ...:         [5.5, 1],
-   ...:         [1, 1],
-   ...:     ]
-   ...: )
+
+
+input_vectors = np.array(
+    [
+         [3, 1.5],
+         [2, 1],
+         [4, 1.5],
+         [3, 4],
+         [3.5, 0.5],
+         [2, 0.5],
+         [5.5, 1],
+         [1, 1],
+     ]
+)
 
 targets = np.array([0, 1, 0, 1, 0, 1, 1, 0])
 
